a6765556ec5ceebde6589301c5df8af0175236fc
Roxenne Lourdes Rosario <lourdesroxenne@gmail.com>
2014-03-09 11:52:12 +0800

    Introduction Assets
    Ito lang yung sinend ko sa mail.

---
+++ Assets/Scripts/Introduction_Scripts/Create.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var createNormal : Texture2D;
+var createHover : Texture2D;
+
+function Start () {
+
+}
+
+function OnMouseEnter () {
+	guiTexture.texture = createHover;
+}
+
+function OnMouseExit () {
+	guiTexture.texture = createNormal;
+}
+
+function OnMouseDown () {
+	Application.LoadLevel("SceneCreation");
+}
---
+++ Assets/Scripts/Introduction_Scripts/Exit.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var exitNormal : Texture2D;
+var exitHover : Texture2D;
+
+function Start () {
+
+}
+
+function OnMouseEnter () {
+	guiTexture.texture = exitHover;
+}
+
+function OnMouseExit () {
+	guiTexture.texture = exitNormal;
+}
+
+function OnMouseDown () {
+	Application.Quit();
+}
